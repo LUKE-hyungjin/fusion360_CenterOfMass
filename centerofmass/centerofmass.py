c37494f444164e5345ea3f0d83b8ec928fdbc3ba
LUKE (HYUNGJIN LEE) <68213792+LUKE-hyungjin@users.noreply.github.com>
2022-07-19 16:10:48 +0900

    Add files via upload

---
+++ centerofmass/centerofmass.py
@@ -0,0 +1,36 @@
+#Author-Leehyungjin
+#Description-change origin to center of mass
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        title = "Center of Mass"
+        product = app.activeProduct
+        design = adsk.fusion.Design.cast(product)
+        root = design.rootComponent
+        features = root.features
+        allOccs = root.occurrences
+        for occs in allOccs:
+            prop = occs.getPhysicalProperties(adsk.fusion.CalculationAccuracy.VeryHighCalculationAccuracy)
+            center_of_mass = [_/100.0 for _ in prop.centerOfMass.asArray()]
+            name = occs.component.name
+            ui.messageBox(f'component name : {name}\nx : {round(center_of_mass[0],5)}m, y : {round(center_of_mass[1],5)}m, z : {round(center_of_mass[2],5)}m\nx : {round(center_of_mass[0]*1000,5)}mm, y : {round(center_of_mass[1]*1000,5)}mm, z : {round(center_of_mass[2]*1000,5)}mm', title)
+            try:
+                selection = occs.component.bRepBodies.item(0)
+                bodies = adsk.core.ObjectCollection.create()
+                bodies.add(selection)
+                vector = adsk.core.Vector3D.create(-100*round(center_of_mass[0],5), -100*round(center_of_mass[1],5), -100*round(center_of_mass[2],5))
+                transform = adsk.core.Matrix3D.create()
+                transform.translation = vector
+                moveFeats = occs.component.features.moveFeatures
+                moveFeatureInput = moveFeats.createInput(bodies, transform)
+                moveFeats.add(moveFeatureInput)
+            except:
+                pass
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
